2af24fe86cb382131e525b59f1808766dc2a1bec
pwdebruin1 <pwdebruin@outlook.com>
2018-06-16 13:25:46 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,6 @@
 curl -sSL https://raw.githubusercontent.com/pwdebruin1/nginx-config/master/html/index.html > /var/www/html/index.html
 
 # Set default site config which includes rewrite to HTTPS behind ALB
-curl -sSL https://raw.githubusercontent.com/pwdebruin1/nginx-config/master/config/private > /etc/nginx/sites-enabled/private
-cp /etc/nginx/sites-available/private /etc/nginx/sites-available/private
+curl -sSL https://raw.githubusercontent.com/pwdebruin1/nginx-config/master/config/private > /etc/nginx/sites-available/private
+cp /etc/nginx/sites-available/private /etc/nginx/sites-enabled/private
 systemctl reload nginx
